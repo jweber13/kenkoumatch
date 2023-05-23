13926dc3a76e2433d178e1c31714c05cb85c9696
Ryo Tamiya <ryo.tamiyaayimat.oyr@gmail.com>
2023-05-23 18:51:52 +0900

    set up deepl(with gem deepl-rb)

--- config/initializers/deepl.rb
+++ config/initializers/deepl.rb
@@ -1,3 +1,5 @@
+require 'deepl'
+
 DeepL.configure do |config|
   config.auth_key = ENV.fetch("DEEPL_AUTH_KEY")
   config.host = 'https://api-free.deepl.com' # Default value is 'https://api.deepl.com'
