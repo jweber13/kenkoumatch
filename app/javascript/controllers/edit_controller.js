28369a7991e36b331b7d7c12ca52320dd8806899
Mathieu Belson <mathieu.belson@gmail.com>
2023-05-30 11:17:55 +0900

    loading spinner added to redo keywords and redo phrases

--- app/javascript/controllers/edit_controller.js
+++ app/javascript/controllers/edit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit"
 export default class extends Controller {
-  static targets = ["edited"]
+  static targets = ["edited","spinner","overlay"]
   static values = {
     id: String
    }
@@ -19,11 +19,24 @@ export default class extends Controller {
     // const url = `${window.location.href}/redo`
     const url = `${window.location.origin}/cards/${this.idValue}/redo`
     console.log(url);
+    this.showLoadingSpinner();
     fetch(url, { method: "POST", headers: { 'Accept': 'text/plain' } } )
     .then(response => response.text())
     .then((data) => {
       // console.log(data);
+      this.hideLoadingSpinner();
       this.editedTarget.innerHTML = data
     })
+  }
+
+  showLoadingSpinner() {
+    console.log("button clicked");
+    this.spinnerTarget.innerHTML = "<div class='text-center'><div class='spinner-border border-xl text-info' style='width: 8rem; height: 8rem;' role='status'> <span class='sr-only'>Loading...</span></div></div>";
+    this.overlayTarget.style.display = "block"; // show overlay
+  }
+
+  hideLoadingSpinner() {
+    this.spinnerTarget.innerHTML = ""; // remove spinner HTML
+    this.overlayTarget.style.display = "none"; // hide overlay
   }
 }
